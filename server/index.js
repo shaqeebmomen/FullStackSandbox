08ea8452600fa0ba7fbb7ce196092fc534ce8185
Shaqeeb Momen <shaqeebmomen@users.noreply.github.com>
2020-09-01 19:21:39 -0400

    support for front end serving

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const app = express();
 const postRoutes = require('./routes/api/postRoutes');
 
-
 // use port assigned to hosting environment or 5000 if testing
 const port = process.env.PORT || 3000;
 
@@ -19,10 +18,24 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         });
     });
 
-// Middleware
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+// Middleware for all requestes
+app.use(express.urlencoded({ extended: true })); // parses form data
+app.use(express.json()); // parses json data
 app.use(cors());
 
-// Router
+// API Routes
 app.use('/api/posts', postRoutes);
+
+// Handle production routes and front end routes
+if (process.env.NODE_ENV === 'production') {
+    // Static (built) folder
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handling singe page application (the kind of app vue makes)
+    app.get(/.*/, (req, res) => {
+        res.sendFile(__dirname + '/public/index.html')
+    });
+}
+
+
+
